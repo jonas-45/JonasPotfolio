04a40057527ca2c3317d9b75af0430c8f8883c14
jonas-45 <jonaswullah@gmail.com>
2022-12-28 16:34:40 +0000

    Add array to dynamically fill values in projects section

---
+++ popUpScript.js
@@ -0,0 +1,27 @@
+let m_technologies = ["html","css","javascript"];
+
+let m_name1 = "Tonic";
+let m_description1 = "A daily selection of privately personalized reads; no accounts or sign-ups required.";
+let m_img_url1 = "./images/card1.png";
+//let m_technologies1 = ["html","css","javascript"];
+let mobile_project1 = {name:m_name1, description:m_description1, image_url:m_img_url1, technologies:m_technologies}
+
+let m_name2 = "Multi-Post Stories";
+let m_description2 = "A daily selection of privately personalized reads; no accounts or sign-ups required.";
+let m_img_url2 = "./images/card2.png";
+//let m_technologies2 = ["html","css","javascript"];
+let mobile_project2 = {name:m_name2, description:m_description2, image_url:m_img_url2, technologies:m_technologies}
+
+let m_name3 = "Tonic";
+let m_description3 = "A daily selection of privately personalized reads; no accounts or sign-ups required.";
+let m_img_url3 = "./images/card3.png";
+//let m_technologies2 = ["html","css","javascript"];
+let mobile_project3 = {name:m_name3, description:m_description3, image_url:m_img_url3, technologies:m_technologies}
+
+let m_name4 = "Multi-Post Stories";
+let m_description4 = "A daily selection of privately personalized reads; no accounts or sign-ups required.";
+let m_img_url4 = "./images/card4.png";
+//let m_technologies2 = ["html","css","javascript"];
+let mobile_project4 = {name:m_name4, description:m_description4, image_url:m_img_url4, technologies:m_technologies}
+
+let mobileProjects = [mobile_project1,mobile_project2,mobile_project3,mobile_project4];
