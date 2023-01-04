769222c4a20ba7969e9d90615fa128f867bbc7e3
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-01-04 08:51:14 +0100

    Add function to update project section data

--- popUpScript.js
+++ popUpScript.js
@@ -46,3 +46,44 @@ let technologies = document.querySelectorAll(".web-technologies ul");
 let project_details_btn = document.querySelectorAll(".project-btn");
 
 let dot_img = "./images/counter.png";
+
+ProjectsData.forEach((project,index) => {
+  let project_card_image = document.createElement("img");
+  project_card_image.src = project.imageUrl;
+  
+  
+
+  card_img[index].appendChild(project_card_image);
+  titles[index].innerHTML = project["title"];
+  description[index].innerHTML = project["description"];
+
+  project["subtitle"].forEach((subtitle,sindex) => {
+    if(sindex === project["subtitle"].length - 1){
+      let subtitle_item = document.createElement("li");
+      subtitle_item.innerHTML = subtitle;
+      subtitles[index].appendChild(subtitle_item);
+    }else{
+      let subtitle_item = document.createElement("li");
+      subtitle_item.innerHTML = subtitle;
+      let subtitle_item2 = document.createElement("li");
+      let dot = document.createElement("img");
+      dot.src = dot_img;
+
+      subtitle_item2.appendChild(dot);
+
+      subtitles[index].appendChild(subtitle_item);
+      subtitles[index].appendChild(subtitle_item2);
+    }
+  });
+
+  project["technologies"].forEach((tag) => {
+    let tech_item = document.createElement("li");
+    tech_item.innerHTML = tag;
+    technologies[index].appendChild(tech_item);
+  });
+
+  project_details_btn[index].addEventListener("click", () => {
+    showProjectDetailsPopUp(project);
+  })
+
+});
