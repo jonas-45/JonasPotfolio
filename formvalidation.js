d0d0717313d09772788f7bdd51d1a9c62fec59e5
Frempong <samankwaa.frempong@gmail.com>
2023-01-05 15:04:47 +0000

    Debug formvalidation.js to correct UpperCase

--- formvalidation.js
+++ formvalidation.js
@@ -2,15 +2,22 @@ function validateEmail() {
   // Get the value of the email field
   const email = document.getElementById("email").value;
   const form = document.getElementById("form");
-
+  const errorParagraph = document.querySelector(".form-error");
+  if (errorParagraph.querySelector('p') !== null){
+    const paragraph = errorParagraph.querySelector('p');
+    errorParagraph.removeChild(paragraph);
+    // form.removeAttribute('onsubmit','preventDefault()');
+  } 
+  
   // Check if the email is in lower case
   if (email.toLowerCase() !== email) {
     // If the email is not in lower case, show an error message
     const errorMsg = "Please your email address should be in lower casing"
-    document.querySelector(".form-error").appendChild(document.createElement("p")).innerHTML = errorMsg;
-    
-    preventDefault();
+    errorParagraph.appendChild(document.createElement("p")).innerHTML = errorMsg;
     // Prevent the form from being submitted
+    // form.setAttribute('onsubmit','preventDefault()');
+    // form.setAttribute('onsubmit','validateEmail()');
+    alert('lets see');
     return false;
   }
 
