7e0259ab14fd49345602e6d49fba7f49a8e5a1c5
jonas-45 <jonaswullah@gmail.com>
2023-01-06 12:53:30 +0000

    Remove console statements from localstorage.js

--- localstorage.js
+++ localstorage.js
@@ -1,62 +1,56 @@
-setFormInputValuesFromLocalStorage();
-
 function storageAvailable(type) {
   let storage;
   try {
-      storage = window[type];
-      const x = '__storage_test__';
-      storage.setItem(x, x);
-      storage.removeItem(x);
-      return true;
-  }
-  catch (e) {
-      return e instanceof DOMException && (
-          // everything except Firefox
-          e.code === 22 ||
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+    // everything except Firefox
+      e.code === 22
           // Firefox
-          e.code === 1014 ||
+          || e.code === 1014
           // test name field too, because code might not be present
           // everything except Firefox
-          e.name === 'QuotaExceededError' ||
+          || e.name === 'QuotaExceededError'
           // Firefox
-          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
           // acknowledge QuotaExceededError only if there's something already stored
-          (storage && storage.length !== 0);
+          && (storage && storage.length !== 0);
   }
 }
 
-//Function to retrieve contact form values from local storage
-function setFormInputValuesFromLocalStorage(){
-  if(localStorage.getItem("userInfo")){
-    let udata = JSON.parse(localStorage.getItem("userInfo"));
-    console.log("User data: " + udata);
-
-    document.getElementById("username").value = udata.name;
-    document.getElementById("email").value = udata.email;
-    document.getElementById("message").value = udata.msg;
+// Function to retrieve contact form values from local storage
+function setFormInputValuesFromLocalStorage() {
+  if (localStorage.getItem('userInfo')) {
+    const udata = JSON.parse(localStorage.getItem('userInfo'));
+    document.getElementById('username').value = udata.name;
+    document.getElementById('email').value = udata.email;
+    document.getElementById('message').value = udata.msg;
   }
 }
 
-function addValuesToLocalStorage(inputType){
-  let info = document.getElementById(inputType).value;
-  console.log("INPUT VALUE: " + info);
-  if(storageAvailable("localStorage")){
+function addValuesToLocalStorage(inputType) {
+  const info = document.getElementById(inputType).value;
+
+  if (storageAvailable('localStorage')) {
     let data = {};
-    if(localStorage.getItem("userInfo")){
-      data = JSON.parse(localStorage.getItem("userInfo"));
+    if (localStorage.getItem('userInfo')) {
+      data = JSON.parse(localStorage.getItem('userInfo'));
     }
-    
-    console.log("Object keys: " + Object.keys(data));
-      if(inputType === "username"){
-        data['name'] = info;
-      }else if(inputType == "email"){
-        data.email = info;
-      }else{
-        data.msg = info;
-      }
-  
-      console.log("Info VALUE: " + typeof(data));
-      localStorage.setItem("userInfo",JSON.stringify(data));
-    
+
+    if (inputType === 'username') {
+      data.name = info;
+    } else if (inputType === 'email') {
+      data.email = info;
+    } else {
+      data.msg = info;
+    }
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
   }
 }
+
+setFormInputValuesFromLocalStorage();
